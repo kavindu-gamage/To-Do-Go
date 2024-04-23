28b238736e3e582105f32a3b6f8b1edc00a10507
Kavindu <kavindu2308@gmail.com>
2024-04-23 11:08:11 +0530

    go init

---
+++ model/taskModel.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Task struct {
+	gorm.Model
+
+	ID int `json:"id"`
+	TaskName    string `json:"name"`
+	Description string `json:"description"`
+	Priority    string `json:"priority"`
+	Status      string `json:"status"`
+	DueDate     string `json:"due_date"`
+}
+
+
+//This gorm popular for object relational mapping
+//used to simplify database operations by mapping go structs to datavse tables and providing methods to interact with database
